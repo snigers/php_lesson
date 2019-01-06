411565b9fedc55bbc2b7a3171860eb4f20c11be3
snigers <educon@yandex.ru>
2019-01-06 14:18:48 +0300

    php-lesson 5-6

---
+++ php/5/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Lesson 5-6</title>
+  </head>
+  <body>
+
+    <?php
+      /* $a = 20;
+      $b = $a;
+      var_dump($a);
+      $a = 7;
+      var_dump($a);
+      var_dump($b); */
+
+      //echo $a + $b;
+      /* $a = 4;
+      $b = &$a;
+      var_dump($a);
+      $a = 7;
+      var_dump($a);
+      var_dump($b); */
+
+      // echo 'test' + 4;
+
+      /* $a = 3;
+      $b = 4;
+      var_dump($a);
+      var_dump(++$a); */
+
+      /* $str1 = 'Hello, ';
+      $str2 = 'World!';
+      $str1 .= ' World!';
+      echo $str1 . $str2; */
+
+      /* $a = 5;
+      // $a = $a + 2;
+      $a *= 2;
+      var_dump($a); */
+
+      /* NULL;
+      
+      $var = NULL;
+      unset($var);
+      var_dump(++$var); */
+
+      
+
+    ?>
+
+  </body>
+</html>

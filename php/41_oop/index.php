f73b1ecbd8e14c3bca7fb646cf094e5e9b8ded69
snigers <educon@yandex.ru>
2019-04-02 22:15:41 +0300

    39-41

---
+++ php/41_oop/index.php
@@ -0,0 +1,46 @@
+<?php
+
+use \app\{NoteBookProduct, BookProduct};
+use \wfm\interfaces\IGadget;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+/* error_reporting(-1);
+
+function autoloder1($class) {
+  echo $class . '<br>';
+  $class = str_replace("\\", '/', $class);
+  $file = __DIR__ . "/{$class}.php";
+  if (file_exists($file)) {
+    require_once $file;
+  }
+}
+
+
+spl_autoload_register('autoloder1'); */
+
+function debug($data) {
+  echo '<pre>' . print_r($data,1) . '</pre>';
+}
+
+function offerCase(IGadget $product) {
+  echo "<p>Предлагаем чехол для гаджета {$product->getName()}</p>";
+}
+
+$book = new BookProduct('Три мушкитера', 20, 1000);
+$notebook = new NotebookProduct('Dell', 20, 'Intel');
+
+// offerCase($notebook);
+
+// debug($book);
+// debug($notebook);
+
+$a = new \app\A();
+$b = new \app\B();
+
+$a->getTest();
+$b->getTest();
+$b->getTest2();
+
+$book->doAction1()->doAction2();
